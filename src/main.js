b6b79ac714f342e939ece48e515916d57f5c9142
andimalfresh <w.andrew.pedersen@gmail.com>
2018-09-14 14:57:16 -0600

    Loops and Conditionals Check point completed and passed

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 // Write a for loop that will print only even numbers from an array:
 // For example: question0([ 5, 20, 11, 42, 2, 19 ]) >> [ 20, 42, 2 ]
 
-function question0 (array) {
-  // Write your code here! Make sure not to modify the function
-  // declaration above or the `module.exports` code below
+function question0(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      console.log(array[i])
+    }
+  }
 }
 
 // Write a for loop that doubles each number in the following array. If the
 // resulting number is greater than 12, print it to the console:
 // For example: [ 2, 4, 8, 16 ] would print the numbers 16 and 32
 
-function question1 (array) {
-  // Write your code here! Make sure not to modify the function
-  // declaration above or the `module.exports` code below
+function question1(array) {
+  for (let i = 0; i < array.length; i++) {
+    let newAnswer = array[i] * 2
+    if (newAnswer > 12) {
+      console.log(newAnswer)
+    }
+  }
 }
 
 // Write a for loop to iterate over every element in the following array:
@@ -24,9 +31,18 @@ function question1 (array) {
 // - If the day is Monday, print "I hate Mondays more than Garfield."
 // - Otherwise, print "Everybody's working for the weekend."
 
-function question2 (array) {
-  // Write your code here! Make sure not to modify the function
-  // declaration above or the `module.exports` code below
+function question2(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'Sat' || array[i] === 'Sun') {
+      console.log('Yay, it\'s the weekend!')
+    } else if (array[i] === 'Fri') {
+      console.log('Almost there...')
+    } else if (array[i] === 'Mon') {
+      console.log('I hate Mondays more than Garfield.')
+    } else {
+      console.log('Everybody\'s working for the weekend.')
+    }
+  }
 }
 
 module.exports = { question0, question1, question2 }
